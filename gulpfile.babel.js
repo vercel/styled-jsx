1a43f63d81c531e82de98142bdb075afee7f42a7
nkzawa <naoyuki.kanezawa@gmail.com>
2016-12-12 13:00:19 +0900

    add a task to get runtime size

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,21 @@
-// Packages
+import {join} from 'path'
 import gulp from 'gulp'
 import babel from 'gulp-babel'
+import {minify} from 'uglify-js'
+import size from 'human-size'
 
-gulp.task('transpile', () =>
+gulp.task('transpile', () => {
   gulp.src('src/**/*.js')
   .pipe(babel())
-  .pipe(gulp.dest('dist')))
+  .pipe(gulp.dest('dist'))
+})
+
+gulp.task('runtime-size', ['transpile'], () => {
+  const files = ['flush.js', 'memory.js', 'render.js', 'style.js']
+  const {code} = minify(files.map(f => join(__dirname, 'dist', f)))
+  console.log('files:', files.join(', '))
+  console.log('minified:', size(code.length))
+})
 
 gulp.task('watch', () => gulp.watch('src/*', ['transpile']))
 gulp.task('default', ['transpile', 'watch'])
