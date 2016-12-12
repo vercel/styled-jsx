c5c4ade54d787cc9611d65e2863f0d199b486670
nkzawa <naoyuki.kanezawa@gmail.com>
2016-12-13 01:16:53 +0900

    use babili instead of uglify-js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,9 @@
+import 'babel-polyfill'
 import {join} from 'path'
+import {gzipSync} from 'zlib'
 import gulp from 'gulp'
 import babel from 'gulp-babel'
-import {minify} from 'uglify-js'
+import {transformFile} from 'babel-core'
 import size from 'human-size'
 
 gulp.task('transpile', () => {
@@ -10,11 +12,33 @@ gulp.task('transpile', () => {
   .pipe(gulp.dest('dist'))
 })
 
-gulp.task('runtime-size', ['transpile'], () => {
+gulp.task('runtime-size', async () => {
   const files = ['flush.js', 'memory.js', 'render.js', 'style.js']
-  const {code} = minify(files.map(f => join(__dirname, 'dist', f)))
+
+  const result = await Promise.all(files
+  .map(f => join(__dirname, 'src', f))
+  .map(transform))
+
+  const code = result.map(({code}) => code).join('')
+
+  console.log('-----------------------------------------------')
   console.log('files:', files.join(', '))
   console.log('minified:', size(code.length))
+  console.log('minified and gzipped:', size(gzipSync(code).length))
+  console.log('-----------------------------------------------')
+
+  function transform (file) {
+    return new Promise((resolve, reject) => {
+      transformFile(file, {
+        presets: ['babili']
+      }, (err, data) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(data)
+      })
+    })
+  }
 })
 
 gulp.task('watch', () => gulp.watch('src/*', ['transpile']))
