66d23052c09859ede3d0f89cf3a7823b0b924715
Giuseppe <giuseppeg@users.noreply.github.com>
2017-01-17 19:56:41 +0100

    nit

--- benchmark/babel.js
+++ benchmark/babel.js
@@ -1,11 +1,11 @@
 import {resolve} from 'path'
 import Benchmark from 'benchmark'
 import {transform as babel} from 'babel-core'
-import fs from 'fs'
+import {readFileSync} from 'fs'
 
 import plugin from '../src/babel'
 
-const read = path => fs.readFileSync(resolve(__dirname, path), 'utf8')
+const read = path => readFileSync(resolve(__dirname, path), 'utf8')
 const fixture = read('./fixtures/babel.js')
 
 module.exports = new Benchmark({
