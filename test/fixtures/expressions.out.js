13858a2b9952f3c292e5c8d1a39fb4fbd361e917
nkzawa <naoyuki.kanezawa@gmail.com>
2017-01-25 22:35:18 +0900

    fix test fixture

--- test/fixtures/expressions.out.js
+++ test/fixtures/expressions.out.js
@@ -5,11 +5,11 @@ const mediumScreen = '680px';
 
 export default (() => <div data-jsx={2520901095}>
     <p data-jsx={2520901095}>test</p>
-    <_JSXStyle styleId={414042974} css={`p.${ color }[data-jsx="2520901095"] {color: ${ otherColor } }`} />
+    <_JSXStyle styleId={414042974} css={`p.${color}[data-jsx="2520901095"] {color: ${otherColor} }`} />
     <_JSXStyle styleId={188072295} css={"p[data-jsx=\"2520901095\"] {color: red }"} />
-    <_JSXStyle styleId={806016056} css={`body { background: ${ color } }`} />
-    <_JSXStyle styleId={924167211} css={`p[data-jsx="2520901095"] {color: ${ color } }`} />
-    <_JSXStyle styleId={3469794077} css={`p[data-jsx="2520901095"] {color: ${ darken(color) } }`} />
-    <_JSXStyle styleId={945380644} css={`p[data-jsx="2520901095"] {color: ${ darken(color) + 2 } }`} />
-    <_JSXStyle styleId={4106311606} css={`@media (min-width: ${ mediumScreen }) {p[data-jsx="2520901095"] {color: green }p[data-jsx="2520901095"] {color ${ `red` }}}p[data-jsx="2520901095"] {color: red }`} />
+    <_JSXStyle styleId={806016056} css={`body { background: ${color} }`} />
+    <_JSXStyle styleId={924167211} css={`p[data-jsx="2520901095"] {color: ${color} }`} />
+    <_JSXStyle styleId={3469794077} css={`p[data-jsx="2520901095"] {color: ${darken(color)} }`} />
+    <_JSXStyle styleId={945380644} css={`p[data-jsx="2520901095"] {color: ${darken(color) + 2} }`} />
+    <_JSXStyle styleId={4106311606} css={`@media (min-width: ${mediumScreen}) {p[data-jsx="2520901095"] {color: green }p[data-jsx="2520901095"] {color ${`red`}}}p[data-jsx="2520901095"] {color: red }`} />
   </div>);
