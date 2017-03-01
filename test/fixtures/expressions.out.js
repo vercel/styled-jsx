d416c7b4d38905677f829945e1fce8d3f9b03d6f
nikvm <mail@nikvm.me>
2017-03-01 13:54:20 +0100

    Update test fixtures

--- test/fixtures/expressions.out.js
+++ test/fixtures/expressions.out.js
@@ -4,16 +4,16 @@ const otherColor = 'green';
 const mediumScreen = '680px';
 const animationDuration = '200ms';
 
-export default (() => <div data-jsx={3422891701}>
-    <p data-jsx={3422891701}>test</p>
-    <_JSXStyle styleId={414042974} css={`p.${color}[data-jsx="3422891701"] {color: ${otherColor} }`} />
-    <_JSXStyle styleId={188072295} css={"p[data-jsx=\"3422891701\"] {color: red }"} />
-    <_JSXStyle styleId={806016056} css={`body { background: ${color} }`} />
-    <_JSXStyle styleId={2278229016} css={`body { background: ${color} }`} />
-    <_JSXStyle styleId={924167211} css={`p[data-jsx="3422891701"] {color: ${color} }`} />
-    <_JSXStyle styleId={1586014475} css={`p[data-jsx="3422891701"] {color: ${color} }`} />
-    <_JSXStyle styleId={3469794077} css={`p[data-jsx="3422891701"] {color: ${darken(color)} }`} />
-    <_JSXStyle styleId={945380644} css={`p[data-jsx="3422891701"] {color: ${darken(color) + 2} }`} />
-    <_JSXStyle styleId={4106311606} css={`@media (min-width: ${mediumScreen}) {p[data-jsx="3422891701"] {color: green }p[data-jsx="3422891701"] {color ${`red`}}}p[data-jsx="3422891701"] {color: red }`} />
-    <_JSXStyle styleId={2369334310} css={`p[data-jsx="3422891701"] {-webkit-animation-duration:${animationDuration};animation-duration:${animationDuration};`} />
+export default (() => <div data-jsx={2226644152}>
+    <p data-jsx={2226644152}>test</p>
+    <_JSXStyle styleId={414042974} css={`p.${color}[data-jsx="2226644152"] {color: ${otherColor} }`} />
+    <_JSXStyle styleId={188072295} css={"p[data-jsx=\"2226644152\"] {color: red }"} />
+    <_JSXStyle styleId={806016056} css={` body {background: ${color} }`} />
+    <_JSXStyle styleId={2278229016} css={` body {background: ${color} }`} />
+    <_JSXStyle styleId={924167211} css={`p[data-jsx="2226644152"] {color: ${color} }`} />
+    <_JSXStyle styleId={1586014475} css={`p[data-jsx="2226644152"] {color: ${color} }`} />
+    <_JSXStyle styleId={3469794077} css={`p[data-jsx="2226644152"] {color: ${darken(color)} }`} />
+    <_JSXStyle styleId={945380644} css={`p[data-jsx="2226644152"] {color: ${darken(color) + 2} }`} />
+    <_JSXStyle styleId={1110687739} css={`@media (min-width: ${mediumScreen}) {p[data-jsx="2226644152"] {color: green }p[data-jsx="2226644152"] {color ${`red`}}}p[data-jsx="2226644152"] {color: red }`} />
+    <_JSXStyle styleId={2369334310} css={`p[data-jsx="2226644152"] {-webkit-animation-duration:${animationDuration};animation-duration:${animationDuration};`} />
   </div>);
