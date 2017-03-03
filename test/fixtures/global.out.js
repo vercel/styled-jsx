c0ed6d4de7c38c00210db06b9a04ee549951dc89
nikvm <mail@nikvm.me>
2017-03-03 14:31:17 +0100

    Remove extra whitespace in global styles

--- test/fixtures/global.out.js
+++ test/fixtures/global.out.js
@@ -1,6 +1,6 @@
 import _JSXStyle from 'styled-jsx/style';
 const Test = () => <div data-jsx={733744743}>
-    <_JSXStyle styleId={733744743} css={" body {color: red      }"} />
+    <_JSXStyle styleId={733744743} css={"body {color: red      }"} />
   </div>;
 
-const Test2 = () => <_JSXStyle styleId={188072295} css={" p {color: red }"} />;
+const Test2 = () => <_JSXStyle styleId={188072295} css={"p {color: red }"} />;
