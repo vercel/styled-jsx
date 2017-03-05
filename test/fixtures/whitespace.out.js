9c0741cbc90ae3c692947e0725d262d05eff4c3c
Sultan Tarimo <sultantarimo@me.com>
2017-03-05 07:20:27 +0300

    fix :global & animation:..}

--- test/fixtures/whitespace.out.js
+++ test/fixtures/whitespace.out.js
@@ -3,5 +3,5 @@ export default (() => <div data-jsx={188072295}>
     <p data-jsx={188072295}>test</p>
     <p data-jsx={188072295}>woot</p>
     <p data-jsx={188072295}>woot</p>
-    <_JSXStyle styleId={188072295} css={"p[data-jsx=\"188072295\"] {color: red }"} />
+    <_JSXStyle styleId={188072295} css={"p[data-jsx=\"188072295\"] {color: red}"} />
   </div>);
