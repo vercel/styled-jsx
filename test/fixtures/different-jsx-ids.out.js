f2c3c2d0eaa747fe967a935781c84bd164d93ea5
Sultan Tarimo <sultantarimo@me.com>
2017-03-05 07:38:37 +0300

    fix :global & animation:..} (#144)

--- test/fixtures/different-jsx-ids.out.js
+++ test/fixtures/different-jsx-ids.out.js
@@ -4,12 +4,12 @@ const otherColor = 'green';
 
 const A = () => <div data-jsx={924167211}>
     <p data-jsx={924167211}>test</p>
-    <_JSXStyle styleId={924167211} css={`p[data-jsx="924167211"] {color: ${color} }`} />
+    <_JSXStyle styleId={924167211} css={`p[data-jsx="924167211"] {color: ${color}}`} />
   </div>;
 
 const B = () => <div data-jsx={45234319}>
     <p data-jsx={45234319}>test</p>
-    <_JSXStyle styleId={45234319} css={`p[data-jsx="45234319"] {color: ${otherColor} }`} />
+    <_JSXStyle styleId={45234319} css={`p[data-jsx="45234319"] {color: ${otherColor}}`} />
   </div>;
 
 export default (() => <div>
--- test/fixtures/expressions.out.js
+++ test/fixtures/expressions.out.js
@@ -6,14 +6,14 @@ const animationDuration = '200ms';
 
 export default (() => <div data-jsx={3422891701}>
     <p data-jsx={3422891701}>test</p>
-    <_JSXStyle styleId={414042974} css={`p.${color}[data-jsx="3422891701"] {color: ${otherColor} }`} />
-    <_JSXStyle styleId={188072295} css={"p[data-jsx=\"3422891701\"] {color: red }"} />
+    <_JSXStyle styleId={414042974} css={`p.${color}[data-jsx="3422891701"] {color: ${otherColor}}`} />
+    <_JSXStyle styleId={188072295} css={"p[data-jsx=\"3422891701\"] {color: red}"} />
     <_JSXStyle styleId={806016056} css={`body { background: ${color} }`} />
     <_JSXStyle styleId={2278229016} css={`body { background: ${color} }`} />
-    <_JSXStyle styleId={924167211} css={`p[data-jsx="3422891701"] {color: ${color} }`} />
-    <_JSXStyle styleId={1586014475} css={`p[data-jsx="3422891701"] {color: ${color} }`} />
-    <_JSXStyle styleId={3469794077} css={`p[data-jsx="3422891701"] {color: ${darken(color)} }`} />
-    <_JSXStyle styleId={945380644} css={`p[data-jsx="3422891701"] {color: ${darken(color) + 2} }`} />
-    <_JSXStyle styleId={4106311606} css={`@media (min-width: ${mediumScreen}) {p[data-jsx="3422891701"] {color: green }p[data-jsx="3422891701"] {color ${`red`}}}p[data-jsx="3422891701"] {color: red }`} />
-    <_JSXStyle styleId={2369334310} css={`p[data-jsx="3422891701"] {-webkit-animation-duration:${animationDuration};animation-duration:${animationDuration};`} />
+    <_JSXStyle styleId={924167211} css={`p[data-jsx="3422891701"] {color: ${color}}`} />
+    <_JSXStyle styleId={1586014475} css={`p[data-jsx="3422891701"] {color: ${color}}`} />
+    <_JSXStyle styleId={3469794077} css={`p[data-jsx="3422891701"] {color: ${darken(color)}}`} />
+    <_JSXStyle styleId={945380644} css={`p[data-jsx="3422891701"] {color: ${darken(color) + 2}}`} />
+    <_JSXStyle styleId={4106311606} css={`@media (min-width: ${mediumScreen}) {p[data-jsx="3422891701"] {color: green}p[data-jsx="3422891701"] {color ${`red`}}}p[data-jsx="3422891701"] {color: red}`} />
+    <_JSXStyle styleId={2369334310} css={`p[data-jsx="3422891701"] {-webkit-animation-duration:${animationDuration};animation-duration:${animationDuration};}`} />
   </div>);
