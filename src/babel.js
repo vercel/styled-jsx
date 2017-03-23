63eef08559bd2337babec2d23ba6d6524ee96c31
soulchainer <soulchainer@gmail.com>
2017-03-23 18:48:01 +0100

    Fix expr namespacing inside animation shorthand
    Changes the placeholder for expressions to avoid namespacing non
    `animation-name` strings when using them into `animation` shorthand.

--- src/babel.js
+++ src/babel.js
@@ -102,11 +102,11 @@ export default function ({types: t}) {
     // e.g.
     // p { color: ${myConstant}; }
     // becomes
-    // p { color: ___styledjsxexpression0___; }
+    // p { color: 0__styledjsxexpression_0__0; }
 
     const replacements = expressions.map((e, id) => ({
       pattern: new RegExp(`\\$\\{\\s*${escapeStringRegExp(e.getSource())}\\s*\\}`),
-      replacement: `___styledjsxexpression_${id}___`,
+      replacement: `0__styledjsxexpression_${id}__0`,
       initial: `$\{${e.getSource()}}`
     })).sort((a, b) => a.initial.length < b.initial.length)
 
