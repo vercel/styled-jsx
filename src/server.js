81ac6a36ba2c52943439a3d9443530e0b8e17091
Roland Warmerdam <rowno@webspirited.com>
2017-05-11 11:00:55 -0700

    Make rendering order deterministic (#187)

--- src/server.js
+++ src/server.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import {flush} from './style'
 
-const {hasOwnProperty} = Object.prototype
-
 export default function flushToReact() {
   const mem = flush()
   const arr = []
-  for (const id in mem) {
-    if (hasOwnProperty.call(mem, id)) {
-      arr.push(React.createElement('style', {
-        id: `__jsx-style-${id}`,
-        // avoid warnings upon render with a key
-        key: `__jsx-style-${id}`,
-        dangerouslySetInnerHTML: {
-          __html: mem[id]
-        }
-      }))
-    }
+  for (const [id, c] of mem) {
+    arr.push(React.createElement('style', {
+      id: `__jsx-style-${id}`,
+      // avoid warnings upon render with a key
+      key: `__jsx-style-${id}`,
+      dangerouslySetInnerHTML: {
+        __html: c.props.css
+      }
+    }))
   }
   return arr
 }
@@ -24,10 +20,8 @@ export default function flushToReact() {
 export function flushToHTML() {
   const mem = flush()
   let html = ''
-  for (const id in mem) {
-    if (hasOwnProperty.call(mem, id)) {
-      html += `<style id="__jsx-style-${id}">${mem[id]}</style>`
-    }
+  for (const [id, c] of mem) {
+    html += `<style id="__jsx-style-${id}">${c.props.css}</style>`
   }
   return html
 }
