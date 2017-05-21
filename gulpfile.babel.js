25243de2fba9de1e572f3bcc388988c0b8ade599
tim <timdeschryver@outlook.com>
2017-05-21 13:31:58 +0200

    removed babel-polyfill, added transform runtime

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,6 @@ import {join} from 'path'
 import {gzipSync} from 'zlib'
 
 // Packages
-// eslint-disable-next-line no-unused-vars
-import _ from 'babel-polyfill'
 import gulp from 'gulp'
 import babel from 'gulp-babel'
 import {transformFile} from 'babel-core'
