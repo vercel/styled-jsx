f9b6dcb177b036d645fdfdcd1974570c8ddd715e
Giuseppe <giuseppeg@users.noreply.github.com>
2017-05-30 20:18:55 +0200

    Fix at-rules nesting (#216)
    Fixes #214

--- test/styles.js
+++ test/styles.js
@@ -14,11 +14,69 @@ test('transpile styles with attributes', async t => {
 })
 
 test('throws when using nesting', t => {
-  t.throws(() => transform('', `div { &:hover { color: red } }`))
+  const fixtures = [
+    `div { &:hover { color: red } }`,
+
+    `div {
+      color: green;
+      &:hover { color: red } }`,
+
+    `:hover { div { color: red } }`,
+
+    `@media all {
+      div {
+        &:hover { color: red }
+      }
+    }`,
+
+    `* { div { color: red }
+      &.test {
+        color: red;
+      }
+    }`,
+
+    `span {}
+      .test {
+        color: red;
+      div& {
+        color: red;
+      }
+    }`
+  ]
+
+  fixtures.forEach(fixture => {
+    t.throws(() => transform('', fixture))
+    t.throws(() => transform('[data-jsx="123"]', fixture))
+  })
 })
 
-test("doesn't throw when using media queries", t => {
-  t.notThrows(() =>
-    transform('', '@media (min-width: 480px) { div { color: red } }')
-  )
+test("doesn't throw when using at-rules", t => {
+  const fixtures = [
+    '@media (min-width: 480px) { div { color: red } }',
+
+    `span {}
+      @media (min-width: 480px) { div { color: red } }`,
+
+    `@media (min-width: 480px) { div { color: red } }
+    span {}`,
+
+    `:hover {}
+      @media (min-width: 480px) { div { color: red } }`,
+
+    `:hover { color: green }
+      @media (min-width: 480px) { div { color: red } }`,
+
+    `@media (min-width: 480px) { div {} }`,
+
+    `@keyframes foo {
+      0% { opacity: 0 }
+      100% { opacity: 1}
+    }
+    `
+  ]
+
+  fixtures.forEach(fixture => {
+    t.notThrows(() => transform('', fixture))
+    t.notThrows(() => transform('[data-jsx="123"]', fixture))
+  })
 })
