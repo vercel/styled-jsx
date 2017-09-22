a3ec074451498ed6dc897f378f0d941f17b4854e
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2017-09-22 14:09:50 +0200

    fix e2e tests

--- test/browser.js
+++ test/browser.js
@@ -1,14 +1,14 @@
 import test from 'ava'
 import React from 'react'
 import ReactJSDOM from 'react-jsdom'
-// eslint-disable-next-line no-unused-vars
-import JSXStyle from '../src/style'
-
-const getStyles = () => [].slice.call(document.querySelectorAll('style'))
-
-const getCSS = () => getStyles().map(s => s.textContent).join('\n')
 
 test('Renders styles and updates them', t => {
+  // Workaround so that JSXStyle works as if it was running on a browser.
+  const originalWindow = global.window
+  global.window = true
+  // eslint-disable-next-line no-unused-vars
+  const JSXStyle = require('../src/style').default
+
   // eslint-disable-next-line no-unused-vars
   class Component extends React.Component {
     constructor(props) {
@@ -18,13 +18,22 @@ test('Renders styles and updates them', t => {
         css: 'div { font-size: 10px }'
       }
     }
+    getStyles() {
+      return [].slice
+        .call(document.querySelectorAll('style'))
+        .map(s => s.textContent)
+        .join('\n')
+    }
     getExpectedStyles() {
       const { styleId, css } = this.state
       return ['/*123*/div { color: red }', `/*${styleId}*/${css}`].join('\n')
     }
     componentDidMount() {
-      t.is(getCSS(), this.getExpectedStyles(), 'styles not rendered correctly')
-
+      t.is(
+        this.getStyles(),
+        this.getExpectedStyles(),
+        'styles not rendered correctly'
+      )
       // Now update styles
       this.setState({
         styleId: 678,
@@ -32,7 +41,11 @@ test('Renders styles and updates them', t => {
       })
     }
     componentDidUpdate() {
-      t.is(getCSS(), this.getExpectedStyles(), 'styles not updated correctly')
+      t.is(
+        this.getStyles(),
+        this.getExpectedStyles(),
+        'styles not updated correctly'
+      )
     }
     render() {
       const { styleId, css } = this.state
@@ -46,4 +59,5 @@ test('Renders styles and updates them', t => {
   }
 
   ReactJSDOM.render(<Component />)
+  global.window = originalWindow
 })
