0a2df07095e9753888691688fbd6e8dd581dcb24
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2017-10-09 15:50:57 +0200

    external styles test

--- test/external.js
+++ test/external.js
@@ -35,23 +35,22 @@ test('(optimized) transpiles external stylesheets (CommonJS modules)', async t =
 })
 
 test('throws when using `this.something` in external stylesheets', async t => {
-  const { message } = await t.throws(
-    transform('./fixtures/styles-external-invalid.js')
-  )
+  const { message } = await t.throws(transform('./fixtures/styles-external-invalid.js'))
   t.regex(message, /this\.props/)
 })
 
 test('throws when referring an undefined value in external stylesheets', async t => {
-  const { message } = await t.throws(
-    transform('./fixtures/styles-external-invalid2.js')
-  )
+  const { message } = await t.throws(transform('./fixtures/styles-external-invalid2.js'))
   t.regex(message, /props\.color/)
 })
 
+test('use external stylesheets', async t => {
+  const { code } = await transform('./fixtures/external-stylesheet.js')
+  t.snapshot(code)
+})
+
 test('use external stylesheets (multi-line)', async t => {
-  const { code } = await transform(
-    './fixtures/external-stylesheet-multi-line.js'
-  )
+  const { code } = await transform('./fixtures/external-stylesheet-multi-line.js')
   t.snapshot(code)
 })
 
