80bf9600995f0b0d72d37250c4c33d07ebb95fdc
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2017-10-18 08:54:50 +0200

    Fix external styles not supporting nested objects for constants

--- test/fixtures/styles.js
+++ test/fixtures/styles.js
@@ -1,15 +1,34 @@
 import css from 'styled-jsx/css'
-
+import colors, { size } from './constants'
 const color = 'red'
 
 const bar = css`
-  div { font-size: 3em }
+  div {
+    font-size: 3em;
+  }
 `
+
+const a = css`
+  div {
+    font-size: ${size}em;
+  }
+`
+
+const b = css`
+  div {
+    color: ${colors.green.light};
+  }
+`
+
 export const uh = bar
 
 export const foo = css`div { color: ${color}}`
 
 export default css`
-  div { font-size: 3em }
-  p { color: ${color};}
+  div {
+    font-size: 3em;
+  }
+  p {
+    color: ${color};
+  }
 `
