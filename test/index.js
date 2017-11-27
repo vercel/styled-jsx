610b301e85c160ee8d8aec03e98cb9baecea882c
zoe <z.zoelec2@gmail.com>
2017-11-27 07:14:18 -0800

    Remove comment

--- test/index.js
+++ test/index.js
@@ -20,16 +20,6 @@ test('works with stateless', async t => {
   t.snapshot(code)
 })
 
-// test('works with complex spread props', async t => {
-//   const { code } = await transform('./fixtures/attribute-generation-classname-rewriting.js')
-//   t.snapshot(code)
-// })
-//
-// test('works with complex spread props (class)', async t => {
-//   const { code } = await transform('./fixtures/attribute-generation-classname-rewriting-with-class.js')
-//   t.snapshot(code)
-// })
-
 test('ignores whitespace around expression container', async t => {
   const { code } = await transform('./fixtures/whitespace.js')
   t.snapshot(code)
