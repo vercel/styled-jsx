09140a4b09f2ad153c957973e44bb9ab537a00b6
zoe <z.zoelec2@gmail.com>
2017-11-28 07:37:39 -0800

    (re)Add tests

--- test/index.js
+++ test/index.js
@@ -20,6 +20,13 @@ test('works with stateless', async t => {
   t.snapshot(code)
 })
 
+test('works with spread', async t => {
+  const { code } = await transform(
+    './fixtures/attribute-generation-classname-rewriting.js'
+  )
+  t.snapshot(code)
+})
+
 test('ignores whitespace around expression container', async t => {
   const { code } = await transform('./fixtures/whitespace.js')
   t.snapshot(code)
