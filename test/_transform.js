d466416b84a357b5609e24b9129d2bff00569af9
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-03-18 18:11:34 +0100

    add more tests

--- test/_transform.js
+++ test/_transform.js
@@ -1,10 +1,27 @@
 import path from 'path'
-import { transformFile } from 'babel-core'
+import { transformFile, transform } from 'babel-core'
 
 export default (file, opts = {}) =>
   new Promise((resolve, reject) => {
     transformFile(
       path.resolve(__dirname, file),
+      {
+        babelrc: false,
+        ...opts
+      },
+      (err, data) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(data)
+      }
+    )
+  })
+
+export const transformSource = (src, opts = {}) =>
+  new Promise((resolve, reject) => {
+    transform(
+      src,
       {
         babelrc: false,
         ...opts
