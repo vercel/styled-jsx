bb98185aec9c9bdac8220601dbed752247ada1bf
Davis <11590024+dav-is@users.noreply.github.com>
2018-08-28 17:23:04 -0500

    Parameter is not required

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { flush } from './style'
 
-export default function flushToReact(nonce) {
+export default function flushToReact(nonce = undefined) {
   return flush().map(args => {
     const id = args[0]
     const css = args[1]
@@ -9,7 +9,7 @@ export default function flushToReact(nonce) {
       id: `__${id}`,
       // Avoid warnings upon render with a key
       key: `__${id}`,
-      nonce: nonce ? nonce : undefined,
+      nonce,
       dangerouslySetInnerHTML: {
         __html: css
       }
@@ -17,7 +17,7 @@ export default function flushToReact(nonce) {
   })
 }
 
-export function flushToHTML(nonce) {
+export function flushToHTML(nonce = undefined) {
   return flush().reduce((html, args) => {
     const id = args[0]
     const css = args[1]
