014ad97c69e36eae830b5940a18dc1c2d5b267d7
JJ Kasper <jj@jjsweb.site>
2019-01-04 11:52:26 -0600

    Updated to remove unnecessary traverse for fragments

--- src/babel.js
+++ src/babel.js
@@ -61,6 +61,15 @@ export default function({ types: t }) {
       },
       JSXElement: {
         enter(path, state) {
+          // since JSXFragments don't visit JSXOpeningElement
+          // increment ignoreClosing for them here
+          if (t.isJSXFragment && t.isJSXFragment(path)) {
+            if (state.ignoreClosing === null) {
+              state.ignoreClosing = 0
+            }
+            state.ignoreClosing++
+          }
+
           if (state.hasJSXStyle !== null) {
             return
           }
@@ -183,18 +192,6 @@ export default function({ types: t }) {
           state.hasJSXStyle = true
           state.file.hasJSXStyle = true
 
-          // JSXOpeningElement isn't called for Fragments so
-          // initialize ignoreClosing here (not supported < babel 7)
-          if (t.isJSXFragment) {
-            t.traverseFast(path.node, p => {
-              if (t.isJSXFragment(p)) {
-                if (state.ignoreClosing === null) {
-                  state.ignoreClosing = 0
-                }
-                state.ignoreClosing++
-              }
-            })
-          }
           // Next visit will be: JSXOpeningElement
         },
         exit(path, state) {
--- test/fixtures/fragment.js
+++ test/fixtures/fragment.js
@@ -1,17 +1,19 @@
+import React from 'react'
+
 export default () => (
   <>
     <p>Testing!!!</p>
     <p className="foo">Bar</p>
     <>
       <h3 id="head">Title...</h3>
-      <>
+      <React.Fragment>
         <p>hello</p>
         <>
           <p>foo</p>
           <p>bar</p>
         </>
         <p>world</p>
-      </>
+      </React.Fragment>
     </>
     <style jsx>{`
       p {
