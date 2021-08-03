5ca0c2c2383d50d1b45feafbf5ef058b96993ef8
Jiachi Liu <inbox@huozhi.im>
2021-08-03 19:55:39 +0800

    revert

--- css.d.ts
+++ css.d.ts
@@ -1,11 +1,11 @@
 // Definitions by: @types/styled-jsx <https://www.npmjs.com/package/@types/styled-jsx>
 
-declare function css(chunks: TemplateStringsArray, ...args: any[]): string
+declare function css(chunks: TemplateStringsArray, ...args: any[]): JSX.Element
 declare namespace css {
-  function global(chunks: TemplateStringsArray, ...args: any[]): string
+  function global(chunks: TemplateStringsArray, ...args: any[]): JSX.Element
   function resolve(
     chunks: TemplateStringsArray,
     ...args: any[]
-  ): { className: string; styles: string }
+  ): { className: string; styles: JSX.Element }
 }
 export = css
