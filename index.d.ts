eaa5461431f81fabe72e1b54311a490fbcd056c7
Jiachi Liu <inbox@huozhi.im>
2021-08-31 16:01:51 +0800

    fix missing file, add new types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,17 @@
-// Definitions by: @types/styled-jsx <https://www.npmjs.com/package/@types/styled-jsx>
-
-import 'react'
-
 declare module 'react' {
   interface StyleHTMLAttributes<T> extends HTMLAttributes<T> {
     jsx?: boolean
     global?: boolean
   }
 }
+
+export type StyleRegistry = {
+  styles(): JSX.Element[]
+  flush(): void
+}
+export function useStyleRegistry(): StyleRegistry
+export function StyleRegistry({
+  children
+}: {
+  children: JSX.Element
+}): JSX.Element
