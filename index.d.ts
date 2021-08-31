058b3d9128d096bc712976be878a10e35c1a5fb9
Jiachi Liu <inbox@huozhi.im>
2021-08-31 17:06:53 +0800

    fix typing

--- index.d.ts
+++ index.d.ts
@@ -6,7 +6,7 @@ declare module 'react' {
 }
 
 export type StyleRegistry = {
-  styles(): JSX.Element[]
+  styles(options: { nonce?: boolean }): JSX.Element
   flush(): void
 }
 export function useStyleRegistry(): StyleRegistry
