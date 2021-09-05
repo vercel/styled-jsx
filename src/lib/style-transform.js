f97b8c5ee73cf81c3b85f95c8c584988c70291a0
Jiachi Liu <inbox@huozhi.im>
2021-09-06 02:24:05 +0800

    upgrade stylis

--- src/lib/style-transform.js
+++ src/lib/style-transform.js
@@ -1,7 +1,12 @@
-const Stylis = require('stylis')
-const stylisRuleSheet = require('stylis-rule-sheet')
-
-const stylis = new Stylis()
+const {
+  compile,
+  serialize,
+  stringify,
+  middleware,
+  rulesheet,
+  prefixer,
+  namespace
+} = require('stylis')
 
 function disableNestingPlugin(...args) {
   let [context, , , parent = [], line, column] = args
@@ -71,16 +76,8 @@ function sourceMapsPlugin(...args) {
  */
 let splitRules = []
 
-const splitRulesPlugin = stylisRuleSheet(rule => {
+const splitRulesPlugin = rulesheet(rule => {
   splitRules.push(rule)
-})
-
-stylis.use(disableNestingPlugin)
-stylis.use(sourceMapsPlugin)
-stylis.use(splitRulesPlugin)
-stylis.set({
-  cascade: false,
-  compress: true
 })
 
 /**
@@ -97,19 +94,29 @@ function transform(hash, styles, settings = {}) {
   filename = settings.filename
   splitRules = []
 
-  stylis.set({
-    prefix:
-      typeof settings.vendorPrefixes === 'boolean'
-        ? settings.vendorPrefixes
-        : true
-  })
-
-  stylis(hash, styles)
+  const usePrefixing =
+    typeof settings.vendorPrefixes === 'boolean'
+      ? settings.vendorPrefixes
+      : true
+  serialize(
+    compile(`${hash}{${styles}}`),
+    middleware(
+      [
+        namespace,
+        usePrefixing && prefixer,
+        stringify,
+        disableNestingPlugin,
+        sourceMapsPlugin,
+        splitRulesPlugin
+      ].filter(Boolean)
+    )
+  )
 
   if (settings.splitRules) {
     return splitRules
   }
 
+  // return str
   return splitRules.join('')
 }
 
