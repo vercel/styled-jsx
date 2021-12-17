5ad1b638cf7dc10c9a1e8975190eb00087296d6b
Jiachi Liu <inbox@huozhi.im>
2021-12-17 21:51:59 +0100

    types: allow string for nonce typing

--- test/index.js
+++ test/index.js
@@ -20,7 +20,7 @@ const flushToHTML = (registry, options = {}) => {
     const id = args[0]
     const css = args[1]
     html += `<style id="__${id}"${
-      options.nonce ? ` nonce="${options.nonce}"` : ''
+      options.nonce ? ` nonce="${options.nonce.toString()}"` : ''
     }>${css}</style>`
     return html
   }, '')
