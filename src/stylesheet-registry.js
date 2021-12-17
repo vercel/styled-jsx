14f9f29971e7ce983de861f3a39e0c8cd12a0942
Jiachi Liu <inbox@huozhi.im>
2021-12-17 22:05:34 +0100

    convert nonce in react

--- src/stylesheet-registry.js
+++ src/stylesheet-registry.js
@@ -11,7 +11,7 @@ function mapRulesToStyle(cssRules, options = {}) {
       id: `__${id}`,
       // Avoid warnings upon render with a key
       key: `__${id}`,
-      nonce: options.nonce ? options.nonce : undefined,
+      nonce: options.nonce ? options.nonce.toString() : undefined,
       dangerouslySetInnerHTML: {
         __html: css
       }
