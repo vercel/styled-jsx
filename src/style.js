3366cfd026bbc1a711f7b62dff9de33951adbbe5
Jiachi Liu <inbox@huozhi.im>
2021-12-22 13:58:13 +0100

    perf: fallback to module level registry in browser (#768)
    * perf: fallback to module level registry in browser
    
    * preeval registry at top level
    
    * use typeof for conditions

--- src/style.js
+++ src/style.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { useStyleRegistry } from './stylesheet-registry'
+import { useStyleRegistry, createStyleRegistry } from './stylesheet-registry'
 import { computeId } from './lib/hash'
 
 // Opt-into the new `useInsertionEffect` API in React 18, fallback to `useLayoutEffect`.
 // https://github.com/reactwg/react-18/discussions/110
 const useInsertionEffect = React.useInsertionEffect || React.useLayoutEffect
+
+const defaultRegistry =
+  typeof window !== 'undefined' ? createStyleRegistry() : undefined
 export default function JSXStyle(props) {
-  const registry = useStyleRegistry()
+  const registry = defaultRegistry ? defaultRegistry : useStyleRegistry()
 
   // If `registry` does not exist, we do nothing here.
   if (!registry) {
