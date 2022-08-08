eb0c51164cbf8342c76344f921dbdb80e6c86743
Jiachi Liu <inbox@huozhi.im>
2022-08-08 18:44:35 +0200

    update css, marco, style

--- css.d.ts
+++ css.d.ts
@@ -1,11 +1,16 @@
 // Definitions by: @types/styled-jsx <https://www.npmjs.com/package/@types/styled-jsx>
 
-declare function css(chunks: TemplateStringsArray, ...args: any[]): JSX.Element
-declare namespace css {
-  function global(chunks: TemplateStringsArray, ...args: any[]): JSX.Element
-  function resolve(
-    chunks: TemplateStringsArray,
-    ...args: any[]
-  ): { className: string; styles: JSX.Element }
+declare module 'styled-jsx/css' {
+  function css(chunks: TemplateStringsArray, ...args: any[]): JSX.Element
+  namespace css {
+    export function global(
+      chunks: TemplateStringsArray,
+      ...args: any[]
+    ): JSX.Element
+    export function resolve(
+      chunks: TemplateStringsArray,
+      ...args: any[]
+    ): { className: string; styles: JSX.Element }
+  }
+  export = css
 }
-export = css
--- macro.d.ts
+++ macro.d.ts
@@ -1,11 +1,13 @@
-declare namespace css {
-  function resolve(
-    chunks: TemplateStringsArray,
-    ...args: any[]
-  ): {
-    className: string
-    styles: JSX.Element
+declare module 'styled-jsx/marco' {
+  namespace marco {
+    function resolve(
+      chunks: TemplateStringsArray,
+      ...args: any[]
+    ): {
+      className: string
+      styles: JSX.Element
+    }
   }
-}
 
-export = css
+  export = marco
+}
